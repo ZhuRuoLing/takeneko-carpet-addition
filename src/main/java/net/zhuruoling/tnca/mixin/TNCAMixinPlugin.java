b882f955f35a229c6872d6afcf5332b315e59b5e
ZhuRuoLing <jkl-963@live.com>
2024-01-14 20:57:50 +0800

    add mixin plugin to avoid mixin conflict

---
+++ src/main/java/net/zhuruoling/tnca/mixin/TNCAMixinPlugin.java
@@ -0,0 +1,49 @@
+package net.zhuruoling.tnca.mixin;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+public class TNCAMixinPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if (Objects.equals(mixinClassName, "net.zhuruoling.tnca.mixin.ServerPlayNetworkHandlerMixin")){
+            return Boolean.parseBoolean(System.getenv("doNotBypassMessageOrderCheck"));
+        }
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+}
