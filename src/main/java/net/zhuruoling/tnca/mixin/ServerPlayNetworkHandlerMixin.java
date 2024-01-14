d3613eb147b4f1add5b942e63afd510c8e2d62f8
ZhuRuoLing <3489382552@qq.com>
2024-01-14 22:28:47 +0800

    it really works

--- src/main/java/net/zhuruoling/tnca/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/net/zhuruoling/tnca/mixin/ServerPlayNetworkHandlerMixin.java
@@ -1,6 +1,8 @@
 package net.zhuruoling.tnca.mixin;
 
 
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.impl.launch.FabricLauncher;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.zhuruoling.tnca.settings.CarpetAdditionSetting;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,20 +12,23 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.time.Instant;
+import java.util.Objects;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public abstract class ServerPlayNetworkHandlerMixin {
+    @Unique
+    private final static String methodName = FabricLoader.getInstance().getMappingResolver().mapMethodName("intermediary", "net.minecraft.class_3244", "method_44160","(Ljava/time/Instant;)Z");
+
     //#if MC >= 11900
     @Redirect(method = "validateMessage", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;isInProperOrder(Ljava/time/Instant;)Z"))
     public boolean redirect(ServerPlayNetworkHandler instance, Instant instant) throws Throwable {
         if (CarpetAdditionSetting.bypassMessageOrderCheck) {
             return true;
         } else {
-            //INVOKEVIRTUAL net/minecraft/server/network/ServerPlayNetworkHandler.isInProperOrder (Ljava/time/Instant;)Z
             return (boolean) MethodHandles.lookup().findVirtual(ServerPlayNetworkHandler.class,
-                    "isInProperOrder",
+                    methodName,
                     MethodType.methodType(boolean.class, Instant.class)
-                    ).invoke(instance, instant);
+            ).invoke(instance, instant);
         }
     }
     //#endif
